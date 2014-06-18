034990ef8e3bda3b4402bf55decddc7760411dd9
Mauro Jr <blessedguy@live.com>
2014-06-18 20:57:19 -0300

    fixes infufg/sade#70 infufg/sade#71 infufg/sade#72

---
+++ src/com/sade/dao/AtividadeDao.java
@@ -0,0 +1,11 @@
+package com.sade.dao;
+
+import com.sade.model.Atividade;
+
+import javax.persistence.EntityManager;
+
+public class AtividadeDao extends DaoAbstrata<Atividade, Long> {
+    public AtividadeDao(EntityManager entityManager) {
+        super(entityManager);
+    }
+}
--- src/com/sade/dao/Dao.java
+++ src/com/sade/dao/Dao.java
@@ -1,14 +1,17 @@
 package com.sade.dao;
 
+import com.sade.model.Persistable;
+
 import java.util.List;
 
 /**
  *
  * @author Eurismar e Mauro
  */
-public interface Dao<T> {
+public interface Dao<T extends Persistable<PK>, PK> {
     public T save(T obj);
     public T update(T obj);
     public boolean delete(T obj);
     public List<T> list();
+    public T get(PK key);
 }
