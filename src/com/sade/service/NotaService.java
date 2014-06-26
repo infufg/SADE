e34e13249f0fb9754b455f870332b06ebb62c532
Mauro Jr <blessedguy@live.com>
2014-06-25 20:45:41 -0400

    Cria calculadora de nota

---
+++ src/com/sade/service/NotaService.java
@@ -0,0 +1,36 @@
+package com.sade.service;
+
+import com.sade.model.Atividade;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class NotaService {
+    public BigDecimal calculaNotaDeAtividade(Atividade atividade) {
+        BigDecimal pontuacao = new BigDecimal(atividade.getPontuacao());
+        BigDecimal quantidade = new BigDecimal(atividade.getQuantidade());
+        BigDecimal divisor = new BigDecimal(atividade.getDivisor());
+
+        BigDecimal limitePontuacao = new BigDecimal(atividade.getLimitePontuacao());
+
+        BigDecimal nota = (pontuacao.multiply(quantidade)).divide(divisor);
+        if (primeiroEhMaiorQueSegundo(nota, limitePontuacao)) {
+            nota = limitePontuacao;
+        }
+
+        return nota;
+    }
+
+    public BigDecimal calculeNota(List<Atividade> atividades) {
+        BigDecimal notaTotal = BigDecimal.ZERO;
+
+        for(Atividade atividade : atividades)
+            notaTotal = notaTotal.add(calculaNotaDeAtividade(atividade));
+
+        return notaTotal;
+    }
+
+    private boolean primeiroEhMaiorQueSegundo(BigDecimal a, BigDecimal b) {
+        return a.compareTo(b) == 1;
+    }
+}
