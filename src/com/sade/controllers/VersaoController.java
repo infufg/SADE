0db89d25fa91aa2379ec4589f937e4acc285ca37
Julliano <julliano.rn@gmail.com>
2014-06-25 21:20:56 -0300

    Criação dos controllers

---
+++ src/com/sade/controllers/VersaoController.java
@@ -0,0 +1,37 @@
+package com.sade.controllers;
+
+import com.sade.model.Versao;
+import java.util.List;
+
+/**
+ *
+ * @author Julliano
+ */
+public class VersaoController {
+    
+    public Versao save(Versao versao){
+        //TODO esperando service
+        return null;
+    }
+    
+    public Versao update(Versao versao){
+        //TODO esperando service
+        return null;
+    }
+    
+    public boolean delete(Versao versao){
+        //TODO esperando service
+        return false;
+    }
+    
+    public List<Versao> list(){
+        //TODO esperando service
+        return null;
+    }
+    
+    public Versao get(Long idVersao){
+        //TODO esperando service
+        return null;
+    }
+}
+
