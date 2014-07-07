dbd24ec94f79a6f543eb64cae1a6095c437f53fb
Mauro Jr <blessedguy@live.com>
2014-07-06 23:03:42 -0300

    Corrige implementação de compareTo de Docente, agora não usa apenas o nome.

--- src/main/java/com/sade/model/Docente.java
+++ src/main/java/com/sade/model/Docente.java
@@ -50,6 +50,40 @@ public class Docente implements Comparable {
 
 	@Override
 	public int compareTo(Object o) {
-		return this.nome.compareTo(((Docente)o).getNome());
+		if(!(o instanceof Docente))
+			throw new ClassCastException();
+		else {
+			if(nome.compareTo(((Docente) o).getNome()) != 0) {
+				return nome.compareTo(((Docente) o).getNome());
+			} else if(id.compareTo(((Docente) o).getId()) != 0) {
+				return id.compareTo(((Docente) o).getId());
+			} else {
+				return 0;
+			}
+		}
+	}
+
+	@Override
+	public boolean equals(Object o) {
+
+		if (this == o) return true;
+		if (!(o instanceof Docente)) return false;
+
+		Docente docente = (Docente) o;
+
+		if (atividades != null ? !atividades.equals(docente.atividades) : docente.atividades != null) return false;
+		if (id != null ? !id.equals(docente.id) : docente.id != null) return false;
+		if (nome != null ? !nome.equals(docente.nome) : docente.nome != null) return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+
+		int result = id != null ? id.hashCode() : 0;
+		result = 31 * result + (nome != null ? nome.hashCode() : 0);
+		result = 31 * result + (atividades != null ? atividades.hashCode() : 0);
+		return result;
 	}
 }
